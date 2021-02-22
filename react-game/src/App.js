101aa94dd2c9c33def42929e2cd6c4c4fd8df8fb
Nastya <asekerich99@gmail.com>
2021-02-22 19:01:53 +0300

    feat: add basic logic

--- react-game/src/App.js
+++ react-game/src/App.js
@@ -1,13 +1,73 @@
 import './App.css';
+import React, { useState, useEffect } from "react";
+import MemoryGame from './components/MemoryGame'
 
 function App() {
+  const [options, setOptions] = useState(null);
+  const [highScore, setHighScore] = useState(0)
+
+
+  useEffect(() => {
+    // Loads when the game starts
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hi?
-        </p>
+        <h1>
+          Memory Game
+        </h1>
+        <div>
+          High Score: {highScore}
+        </div>
       </header>
+      <main>
+        { options === null 
+          ? (
+              <>
+              <button onClick={() => setOptions(12)}>Easy</button>
+              <button onClick={() => setOptions(18)}>Medium</button>
+              <button onClick={() => setOptions(24)}>Hard</button>
+              </>
+            )
+          : (
+              <>
+                <button
+                  onClick={() => {
+                    const prevOptions = options
+                    setOptions(null)
+                    setTimeout(() => {
+                      setOptions(prevOptions)
+                    }, 5)
+                  }}
+                >
+                  Restart
+                </button>
+                <button onClick={() => setOptions(null)}>Main Menu</button>
+              </>
+            )
+        }
+
+        {
+          options
+            ? (
+              <MemoryGame
+                options={options}
+                setOptions={setOptions}
+                highScore={highScore}
+                setHighScore={setHighScore}
+              />
+              ) 
+            : (
+                <h2>Choose a difficulty to begin!</h2>
+              )
+        }
+      </main>
+      <footer>
+        <p>
+          created by J.Ell.
+        </p>
+      </footer>
     </div>
   );
 }
---
+++ react-game/src/components/MemoryGame.jsx
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from "react";
+import  Card from './Card'
+
+const MemoryGame = ( {options, setOptions, highScore, setHighScore} ) => {
+
+  const [game, setGame] = useState([]);
+  const [flippedCount, setFlippedCount] = useState(0);
+  const [flippedIndexes, setFlippedIndexes] = useState([]);
+
+  const colors = [
+    '#ecdb54',
+    '#e34132',
+    '#6ca0dc',
+    '#944743',
+    '#dbb2d1',
+    '#ec9787',
+    '#00a68c',
+    '#645394',
+    '#6c4f3d',
+    '#ebe1df',
+    '#bc6ca7',
+    '#bfd833',
+  ];
+
+  useEffect(() => {
+    const newGame = []
+    for (let i = 0; i < options / 2; i++) {
+      const firstOption = {
+        id: 2 * i,
+        colorId: i,
+        color: colors[i],
+        flipped: false,
+      }
+      const secondOption = {
+        id: 2 * i + 1,
+        colorId: i,
+        color: colors[i],
+        flipped: false,
+      }
+
+      newGame.push(firstOption)
+      newGame.push(secondOption)
+    }
+
+    const shuffledGame = newGame.sort(() => Math.random() - 0.5)
+    setGame(shuffledGame)
+  }, []);
+
+  useEffect(() => {
+    // Loads when the game variable changes
+  }, [game]);
+
+  if (flippedIndexes.length === 2) {
+    const match = game[flippedIndexes[0]].colorId === game[flippedIndexes[1]].colorId;
+
+    if (match) {
+      const newGame = [...game]
+      newGame[flippedIndexes[0]].flipped = true
+      newGame[flippedIndexes[1]].flipped = true
+      setGame(newGame)
+
+      const newIndexes = [...flippedIndexes]
+      newIndexes.push(false)
+      setFlippedIndexes(newIndexes)
+    } else {
+      const newIndexes = [...flippedIndexes]
+      newIndexes.push(true)
+      setFlippedIndexes(newIndexes)
+    }
+  };
+  
+  return (
+    <div>
+      {(game.length === 0)
+        ? 'loading'
+        : <div id="cards">
+            {game.map((card, index) => (
+              <div className="card" key={index}>
+                <Card
+                  id={index}
+                  color={card.color}
+                  game={game}
+                  flippedCount={flippedCount}
+                  setFlippedCount={setFlippedCount}
+                  flippedIndexes={flippedIndexes}
+                  setFlippedIndexes={setFlippedIndexes}
+                />
+              </div>
+            ))}
+          </div>
+      }
+    </div>
+  )
+}
+
+export default MemoryGame;
